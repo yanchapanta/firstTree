47e42bd1e394b101330957e96710d0eda7a0c38c
Unknown <unknown@example.com>
2020-12-12 22:02:40 -0500

    Prototipo de siseño One (Marco)
    Prototipo

--- js/main.js
+++ js/main.js
@@ -2,27 +2,15 @@
 
 let slider=document.querySelector('.slider_content');
 let sliderInd=document.querySelectorAll('.slider_test');
-{
-    // .clientWidth   | significa q me va a tomar el width del slider
-    // si pongo en consola 
-    // $0
-    // <div class=​"slider_test">​…​</div>​
-    //entonces
-    // $0.clientWidth
-    // 646     | me dice el ancho del slider_test
 
-}
 
-// por que contador 1   | porque cuando pase la segunda vez no va ha ser el tamañio del width de una vez tiene que ser de dos
 let contador=1,
     sizeWidth=sliderInd[0].clientWidth,
     intervalo=2000;
-//vuelve ha declarar la variable para que sea dinámica en resposive
 window.addEventListener("resize",()=>{
     sizeWidth=sliderInd[0].clientWidth;
 });
 
-// repeticion que no sotros queramos
 setInterval(function tiempo(){
     slides();
 },intervalo);
@@ -34,9 +22,8 @@ function slides(){
 
     contador++;
 
-    if(contador===sliderInd.length){ //5===5 entonces contador será 0
+    if(contador===sliderInd.length){ 
         contador=0;
-        // El setTimeout solamente se va ha ejecutar una vez
         setTimeout(() => {
             slider.style.transform='translate(0px)';
             slider.style.transition='transform 0s';
